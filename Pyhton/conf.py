e8dd989f3ebff28e493040b9aea1a4c3913be4fa
GaelFG17 <gaelfg1720@gmail.com>
2024-09-09 15:44:17 -0600

    A

--- Pyhton/conf.py
+++ Pyhton/conf.py
@@ -1,5 +1,5 @@
 import os
-
+#app
 class Conf:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:G170122fg#@localhost/Cuest'
